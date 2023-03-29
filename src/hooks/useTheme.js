370ce718893d91c314e8c262bc5828dd84274fc3
Hassan.Ragab <Hassan.Ragab@thermofisher.com>
2023-03-29 12:40:46 +0200

    add useTheme

---
+++ src/hooks/useTheme.js
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+function useTheme({ startingTheme = "light" }) {
+  const [theme, setTheme] = useState(startingTheme);
+
+  function validateTheme(themeValue) {
+    if (themeValue === "dark") {
+      setTheme(themeValue);
+    } else {
+      setTheme("light");
+    }
+  }
+  return { theme, setTheme: validateTheme };
+}
+
+export default useTheme;
